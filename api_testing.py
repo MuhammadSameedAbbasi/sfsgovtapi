e3a9cdcf20d6782997e5b7a18448d6015bcc4ed1
Sameed Abbasi <msameedabbasi@gmail.com>
2023-08-09 22:34:55 +0500

    Web form

--- api_testing.py
+++ api_testing.py
@@ -1,7 +1,7 @@
 import requests
 
 # URL of the API endpoint
-api_url = 'http://localhost:5000/classify'  # Replace with your API URL
+api_url = 'http://127.0.0.1:5000/update_car_price'  # Replace with your API URL
 
 # Input data to send in the request
 input_data = {
@@ -9,7 +9,7 @@ input_data = {
 }
 
 # Send a POST request to the API endpoint
-response = requests.post(api_url, json=input_data)
+response = requests.post(api_url )
 
 # Print the response
-print(response.json())
+print(response)
--- sfs_API.py
+++ sfs_API.py
@@ -1,32 +1,38 @@
-from flask import Flask, request , jsonify
+from flask import Flask, request, jsonify , render_template, redirect, url_for
 
 app = Flask(__name__)
 
-car_dict = {"Cultus": "270.5",
-            "Mehran": "200.5",
-            "Corolla": "400.5",
-            "Civic": "500.5",
-            "City": "450.5"
-            }
+car_dict = {
+    "Cultus": "270.5",
+    "Mehran": "200.5",
+    "Corolla": "400.5",
+    "Civic": "500.5",
+    "City": "450.5"
+}
 
+@app.route('/get_car_price', methods=['GET'])
+def get_car_price():
+    input_data = request.args.get("model")
 
-@app.route('/classify', methods=['Post'])
-def classify():
-    # Get the image file from the request
-    #file = request.files['image']
-
-    try:
-        input_data = request.json["model"]
-        output_data = car_dict.get(input_data, "Not found")
-        response = {input_data: output_data}
-        return jsonify(response), 200
-    except KeyError:
+    if not input_data:
         return jsonify({"error": "Input not provided"}), 400
-    
 
-    # TODO: Load the machine learning model and use it to classify the image
- 
+    output_data = car_dict.get(input_data, "Not found")
+    response = {
+        "car_model": input_data,
+        "price": output_data
+    }
+    return jsonify(response), 200
 
+@app.route('/update_car_price', methods=['GET', 'POST'])
+def update_car_price():
+    if request.method == 'POST':
+        model = request.form['model']
+        price = request.form['price']
+        car_dict[model] = price
+        return redirect(url_for('update_car_price'))
+
+    return render_template('update_car_price.html', car_dict=car_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
