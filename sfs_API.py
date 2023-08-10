d3f288b486bfcf31cc8f645e112c17f37ee703fb
Sameed Abbasi <msameedabbasi@gmail.com>
2023-08-10 17:41:23 +0500

    firebase storage for car categories

--- sfs_API.py
+++ sfs_API.py
@@ -1,23 +1,35 @@
 from flask import Flask, request, jsonify , render_template, redirect, url_for
+import firebase_admin
+from firebase_admin import storage, credentials
+import json
+
 
 app = Flask(__name__)
 
-car_dict = {
+# car_dict = {}
 #     "Cultus": "Small",
 #     "Mehran": "Small",
 #     "Corolla": "Large",
 #     "Civic": "Large",
 #     "City 2007": "Medium"
-}
+# }
 
-@app.route('/', methods=['GET'])
-def get_car_price():
+cred = credentials.Certificate("subsidized-fueling-system-firebase-adminsdk-e5hzp-38a5c23549.json")
 
-    response = {
-        "car_model": 'test',
-        "price": 300000
-    }
-    return jsonify(response), 200
+firebase_admin.initialize_app( cred, {'storageBucket': 'subsidized-fueling-system.appspot.com'})
+
+def load_category_list():
+    bucket = storage.bucket()
+
+    # Download the category_list.json file from Firebase Storage
+    blob = bucket.blob('category_list.json')
+    json_data = blob.download_as_text()
+
+    # Load JSON content into car_dict
+    global car_dict
+    car_dict = json.loads(json_data)
+
+    return jsonify({"message": "Category list loaded successfully"}), 200
 
 
 @app.route('/get_car_price', methods=['GET'])
@@ -34,8 +46,9 @@ def get_car_price():
     }
     return jsonify(response), 200
 
-@app.route('/update_car_price', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def update_car_price():
+    load_category_list()
     if request.method == 'POST':
         model = request.form['model']
         price = request.form['price']
