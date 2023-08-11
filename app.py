7d4613be3c718029838e69d14d09776046274ac3
Sameed Abbasi <msameedabbasi@gmail.com>
2023-08-12 01:41:38 +0500

    update: changed to category and price

--- app.py
+++ app.py
@@ -6,18 +6,42 @@ import json
 
 app = Flask(__name__)
 
-
-# car_dict = {}
-#     "Cultus": "Small",
-#     "Mehran": "Small",
-#     "Corolla": "Large",
-#     "Civic": "Large",
-#     "City 2007": "Medium"
-# }
-
-cred = credentials.Certificate("templates/subsidized-fueling-system-firebase-adminsdk-e5hzp-38a5c23549.json")
+cred = credentials.Certificate("templates\subsidized-fueling-system-firebase-adminsdk-e5hzp-38a5c23549.json")
 
 firebase_admin.initialize_app( cred, {'storageBucket': 'subsidized-fueling-system.appspot.com'})
+
+@app.route('/get_car_category', methods=['GET'])
+def get_car_category():
+    input_data = request.args.get("model")
+
+    if not input_data:
+        return jsonify({"error": "Input not provided"}), 400
+
+    output_data = car_dict.get(input_data, "Not found")
+    response = {
+        
+        "category": input_data,
+        "price": output_data
+    }
+    return jsonify(response), 200
+
+@app.route('/', methods=['GET', 'POST'])
+def update_car_category():
+    load_category_list()
+    if request.method == 'POST':
+        Category = request.form['Category']
+        Price = request.form['Price']
+        car_dict[Category] = Price
+
+        bucket = storage.bucket()
+        blob = bucket.blob('category_list.json')
+        blob.upload_from_string(json.dumps(car_dict))
+
+
+        return redirect(url_for('update_car_category'))
+
+    return render_template('update_car_category.html', car_dict=car_dict)
+
 
 def load_category_list():
     bucket = storage.bucket()
@@ -33,36 +57,6 @@ def load_category_list():
     return jsonify({"message": "Category list loaded successfully"}), 200
 
 
-@app.route('/get_car_category', methods=['GET'])
-def get_car_category():
-    input_data = request.args.get("model")
-
-    if not input_data:
-        return jsonify({"error": "Input not provided"}), 400
-
-    output_data = car_dict.get(input_data, "Not found")
-    response = {
-        "car_model": input_data,
-        "category": output_data
-    }
-    return jsonify(response), 200
-
-@app.route('/', methods=['GET', 'POST'])
-def update_car_category():
-    load_category_list()
-    if request.method == 'POST':
-        model = request.form['model']
-        category = request.form['category']
-        car_dict[model] = category
-
-        bucket = storage.bucket()
-        blob = bucket.blob('category_list.json')
-        blob.upload_from_string(json.dumps(car_dict))
-
-
-        return redirect(url_for('update_car_category'))
-
-    return render_template('update_car_category.html', car_dict=car_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
